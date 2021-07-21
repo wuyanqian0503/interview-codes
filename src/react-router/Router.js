6128adbc820e16359feff10b587edb2cb23ce72b
吴艳倩 <wuyq@xiaoma.wang>
2021-07-21 20:57:16 +0800

    router修复

--- src/react-router/Router.js
+++ src/react-router/Router.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'React'
+import React, {useState, useEffect} from 'react'
 import { history } from './history.js'
 
 export const RouterContext = React.createContext(null);
